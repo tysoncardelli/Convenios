ddc9f89564d6fb3d989ed89149531985e0abce70
tysoncardelli <tyson.cardelli@unet.edu.ve>
2016-01-12 20:43:21 -0430

    Primer commit del repositirio se agregó los archivos iniciales

---
+++ protected/views/site/informacion.php
@@ -0,0 +1,23 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name . ' - About';
+$this->breadcrumbs=array(
+	'Informacion',
+);
+?>
+<div class="container">
+
+<h2>Formatos</h2>
+<p>Acta de intencion</p>
+<p>Ejemplo de convenio Marco</p>
+<p>Ejemplo de convenio Especifico</p>
+<h2>Aspectos Legales</h2>
+<p>Normas y procedimientos</p>
+
+
+<p>This is a "static" page. You may change the content of this page
+by updating the file <code><?php echo __FILE__; ?></code>.</p>
+
+
+</div>
